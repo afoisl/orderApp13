3634a5a6bf49decce21a04d3d95715992b14cf1c
afoisl <ruaql0810@gmail.com>
2024-11-08 16:58:00 +0900

    init commit

---
+++ src/main/java/com/sparta/orderapp13/OrderApp13Application.java
@@ -0,0 +1,13 @@
+package com.sparta.orderapp13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApp13Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApp13Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/orderapp13/OrderApp13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.orderapp13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderApp13ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
