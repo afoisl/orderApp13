c29670eb5a2bb5e36c5e8000e0239d66f17c1115
afoisl <ruaql0810@gmail.com>
2024-11-11 15:14:57 +0900

    Order Dto 생성

---
+++ src/main/java/com/sparta/orderapp13/dto/OrderRequestDto.java
@@ -0,0 +1,25 @@
+package com.sparta.orderapp13.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class OrderRequestDto {
+
+    private String userId;
+    private UUID storeId;
+    private List<orderFoodDto> foodList;
+    private String paymentMethod;
+    private String orderType;
+    private String orderInstructions;
+    private String deliveryAddress;
+    private String deliveryInstructions;
+
+}
---
+++ src/main/java/com/sparta/orderapp13/dto/OrderResponseDto.java
@@ -0,0 +1,11 @@
+package com.sparta.orderapp13.dto;
+
+import com.sparta.orderapp13.entity.Order;
+
+public class OrderResponseDto {
+
+    
+
+    public OrderResponseDto(Order order) {
+    }
+}
