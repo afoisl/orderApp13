ee0827a9c8f9f612b6b1ac0806f49ad05d824543
afoisl <ruaql0810@gmail.com>
2024-11-13 21:59:34 +0900

    주석

--- src/main/java/com/sparta/orderapp13/repository/OrderRepository.java
+++ src/main/java/com/sparta/orderapp13/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package com.sparta.orderapp13.repository;
 
 
 import com.sparta.orderapp13.entity.Order;
+import com.sparta.orderapp13.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
