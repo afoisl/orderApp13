c9a7e1727e5361ef4bc508f823eb28fe8c8d2226
afoisl <ruaql0810@gmail.com>
2024-11-13 22:51:17 +0900

    .http 파일로 테스트하기

--- src/main/java/com/sparta/orderapp13/OrderApp13Application.java
+++ src/main/java/com/sparta/orderapp13/OrderApp13Application.java
@@ -2,7 +2,9 @@ package com.sparta.orderapp13;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+//@EnableJpaAuditing
 @SpringBootApplication
 public class OrderApp13Application {
 
