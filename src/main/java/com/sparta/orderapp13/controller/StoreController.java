216930c40d6466bfc9b64dff0d0dbb2278ff9b13
afoisl <ruaql0810@gmail.com>
2024-11-13 16:57:06 +0900

    Store 등록, 수정, 카테고리별로 가게 조회 구현

---
+++ src/main/java/com/sparta/orderapp13/controller/StoreController.java
@@ -0,0 +1,35 @@
+package com.sparta.orderapp13.controller;
+
+import com.sparta.orderapp13.dto.StoreRequestDto;
+import com.sparta.orderapp13.dto.StoreResponseDto;
+import com.sparta.orderapp13.service.StoreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class StoreController {
+
+    private final StoreService storeService;
+
+    @PostMapping("/stores")
+    public StoreResponseDto enroll(@RequestBody StoreRequestDto requestDto) {
+        return storeService.enroll(requestDto);
+    }
+
+    @GetMapping("/stores")
+    public List<StoreResponseDto> getAll(@RequestParam String category) {
+        return storeService.getAll(category);
+    }
+
+    @PutMapping("/stores/{storeId}")
+    public UUID updateStore(@PathVariable UUID storeId, @RequestBody StoreRequestDto requestDto) {
+        return storeService.update(storeId, requestDto);
+    }
+
+
+}
---
+++ src/main/java/com/sparta/orderapp13/dto/StoreRequestDto.java
@@ -0,0 +1,22 @@
+package com.sparta.orderapp13.dto;
+
+import com.sparta.orderapp13.entity.Category;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class StoreRequestDto {
+
+    private String storeName;
+    private String storeNumber;
+    private String region;
+    private String city;
+    private String detailAddress;
+    private int postalCode;
+    private Category category;
+}
