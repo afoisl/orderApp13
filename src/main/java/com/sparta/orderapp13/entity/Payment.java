270aca1450ab9a7f7a87ac6f539dd503069208eb
afoisl <ruaql0810@gmail.com>
2024-11-13 12:37:39 +0900

    Payment 엔티티 생성

---
+++ src/main/java/com/sparta/orderapp13/entity/Payment.java
@@ -0,0 +1,54 @@
+package com.sparta.orderapp13.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity
+@Table(name = "payment")
+public class Payment {
+
+    @Id
+    @GeneratedValue
+    @UuidGenerator
+    @Column(columnDefinition = "BINARY(16)")
+    private UUID paymentId;
+
+    @OneToOne
+    @JoinColumn
+    private Order order;
+
+    @Column(nullable = false)
+    private int paymentAmount;
+
+    @Enumerated
+    @Column(nullable = false)
+    private PaymentMethod paymentMethod;
+
+    @Enumerated
+    @Column(nullable = false)
+    private PaymentStatus Status;
+
+    @Column
+    private String createdBy;
+
+    @Column
+    private LocalDateTime updatedAt;
+
+    @Column
+    private String updatedBy;
+
+    @Column
+    private LocalDateTime deletedAt;
+
+    @Column
+    private String deletedBy;
+}
