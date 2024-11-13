c18a4ceb55e0238f16cacd4c55879ace6ee3e0e3
afoisl <ruaql0810@gmail.com>
2024-11-13 12:35:50 +0900

    Order 수정

--- src/main/java/com/sparta/orderapp13/entity/Order.java
+++ src/main/java/com/sparta/orderapp13/entity/Order.java
@@ -2,6 +2,7 @@ package com.sparta.orderapp13.entity;
 
 import com.sparta.orderapp13.dto.OrderRequestDto;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Max;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -25,15 +26,15 @@ public class Order {
     @Column(columnDefinition = "BINARY(16)")
     private UUID orderId;
 
-//    @ManyToOne
-//    @JoinColumn(name = "user_id")
-//    private User user;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 
-//    @OneToMany(mappedBy = "order")
-//    private List<StoreOrder> storeOrderList = new ArrayList<>();
+    @OneToMany(mappedBy = "order")
+    private List<StoreOrder> storeOrderList = new ArrayList<>();
 
-//    @OneToOne(mappedBy = "order")
-//    private Payment payment;
+    @OneToOne(mappedBy = "order")
+    private Payment payment;
 
     @Column
     @Enumerated
@@ -43,42 +44,68 @@ public class Order {
     @Enumerated
     private OrderStatus orderStatus;
 
+    @Column
+    @Max(200)
     private String orderInstructions;
 
+    @Column
     private int totalPrice;
 
+    @Column
     private String deliveryAddress;
 
+    @Column
+    @Max(200)
     private String deliveryInstructions;
 
+    @Column
     private LocalDateTime createdAt;
 
+    @Column
     private String createdBy;
 
+    @Column
     private LocalDateTime updatedAt;
 
+    @Column
     private String updatedBy;
 
+    @Column
     private LocalDateTime deletedAt;
 
+    @Column
     private String deletedBy;
 
-//    public Order(OrderRequestDto requestDto, UUID store, int totalPrice) {
-//        this.orderId = UUID.randomUUID();
-////        this.user = user;
-//        try {
-//            this.orderType = OrderType.valueOf(requestDto.getOrderType().toUpperCase());
-//        } catch (IllegalArgumentException e) {
-//            this.orderType = OrderType.ONLINE_ORDER;
-//        }
-//        this.orderStatus = OrderStatus.ORDER_PENDING;
-//        this.orderInstructions = requestDto.getOrderInstructions();
-//        this.deliveryAddress = requestDto.getDeliveryAddress();
-//        this.deliveryInstructions = requestDto.getDeliveryInstructions();
-//        this.totalPrice = totalPrice;
-//        StoreOrder storeOrder = new StoreOrder(orderId, store.getStoreId());
-//        this.storeOrderList.add(storeOrder);
-//    }
+    public Order(OrderRequestDto requestDto, int totalPrice) {
+        this.orderId = UUID.randomUUID();
+        this.user = user;
+        try {
+            this.orderType = OrderType.valueOf(requestDto.getOrderType().toUpperCase());
+        } catch (IllegalArgumentException e) {
+            this.orderType = OrderType.ONLINE_ORDER;
+        }
+        this.orderStatus = OrderStatus.ORDER_PENDING;
+        this.orderInstructions = requestDto.getOrderInstructions();
+        this.deliveryAddress = requestDto.getDeliveryAddress();
+        this.deliveryInstructions = requestDto.getDeliveryInstructions();
+        this.totalPrice = totalPrice;
+        StoreOrder storeOrder = new StoreOrder(orderId, requestDto.getStoreId());
+        this.storeOrderList.add(storeOrder);
+    }
 
+    public void cancel() {
+        this.orderStatus = OrderStatus.ORDER_CANCEL;
+    }
 
+    public void complete() {
+        this.orderStatus = OrderStatus.ORDER_COMPLETED;
+    }
+
+    public void delivering() {
+        this.orderStatus = OrderStatus.ORDER_DELIVERING;
+    }
+
+    public void confirm() {
+        this.orderStatus = OrderStatus.ORDER_CONFIRMED;
+    }
 }
