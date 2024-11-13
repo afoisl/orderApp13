19b2a8558a5d4d43111ade31a5dfe193229459e5
afoisl <ruaql0810@gmail.com>
2024-11-13 12:36:07 +0900

    OrderController 수정

--- src/main/java/com/sparta/orderapp13/controller/OrderController.java
+++ src/main/java/com/sparta/orderapp13/controller/OrderController.java
@@ -1,13 +1,16 @@
 package com.sparta.orderapp13.controller;
 
-import com.sparta.orderapp13.dto.OrderCancelDto;
 import com.sparta.orderapp13.dto.OrderRequestDto;
 import com.sparta.orderapp13.dto.OrderResponseDto;
 import com.sparta.orderapp13.dto.OrderStatusUpdateDto;
+import com.sparta.orderapp13.dto.OrderUpdateDto;
+import com.sparta.orderapp13.security.UserDetailsImpl;
 import com.sparta.orderapp13.service.OrderService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @RestController
@@ -16,27 +19,39 @@ public class OrderController {
 
     private final OrderService orderService;
 
-//    @PostMapping("/orders")
-//    public OrderResponseDto createOrder(@RequestBody OrderRequestDto requestDto) {
-//        return orderService.createOrder(requestDto);
-//    }
-//
-//    @GetMapping("/orders")
-//    public Page<OrderResponseDto> getOrders(
-//            @RequestParam("page") int page,
-//            @RequestParam("size") int size,
-//            @RequestParam("sortBy") String sortBy,
-//            @RequestParam("isAsc") boolean isAsc) {
-//        return orderService.getOrders(user,page - 1, size, sortBy, isAsc);
-//    }
-//
-//    @PatchMapping
-//    public OrderResponseDto updateOrder(@RequestBody OrderStatusUpdateDto updateDto) {
-//        return orderService.updateOrder(updateDto);
-//    }
-//
-//    @DeleteMapping
-//    public void deleteOrder(@RequestBody OrderCancelDto cancelDto) {
-//        orderService.deleteOrder(cancelDto);
-//    }
+    @PostMapping("/orders")
+    public OrderResponseDto createOrder(@RequestBody OrderRequestDto requestDto) {
+        return orderService.createOrder(requestDto);
+    }
+
+    @GetMapping("/orders")
+    public Page<OrderResponseDto> getOrders(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc") boolean isAsc,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+            ) {
+        return orderService.getOrders(userDetails.getUser(),page - 1, size, sortBy, isAsc);
+    }
+
+    @PatchMapping("/order/confirm")
+    public OrderUpdateDto confirmOrder(@RequestBody OrderUpdateDto requestDto) {
+        return orderService.confirmOrder(requestDto);
+    }
+
+    @PatchMapping("/order/delivering")
+    public OrderUpdateDto deliveringOrder(@RequestBody OrderUpdateDto requestDto) {
+        return orderService.deliveringOrder(requestDto);
+    }
+
+    @PatchMapping("/order/complete")
+    public OrderUpdateDto completeOrder(@RequestBody OrderUpdateDto requestDto) {
+        return orderService.completeOrder(requestDto);
+    }
+
+    @PatchMapping("/order/cancel/{orderId}")
+    public void cancelOrder(@PathVariable UUID orderId) {
+        orderService.cancelOrder(orderId);
+    }
 }
